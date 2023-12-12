36fd772debe5f0fd09ba334c8afc31edd3e42281
Efren Leal <efren@Efrens-MacBook-Pro-2.local>
2023-12-12 11:45:49 -0600

    created vars for each hour in java script, and made an array of the hours and their equivalent value, and also added the current date function

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,27 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var hour9 = document.getElementById('hour-9');
+var hour10 = document.getElementById('hour-10');
+var hour11 = document.getElementById('hour-11');
+var hour12 = document.getElementById('hour-12');
+var hour13 = document.getElementById('hour-13');
+var hour14 = document.getElementById('hour-14');
+var hour15 = document.getElementById('hour-15');
+var hour16 = document.getElementById('hour-16');
+var hour17 = document.getElementById('hour-17');
+
+const timeSections = [
+  {name: "9AM", value: 9}, {name: "10AM", value: 10}, {name: "11AM", value: 11},
+  {name: "12PM", value: 12}, {name: "1PM", value: 13}, {name: "2PM", value: 14},
+  {name: "3PM", value: 15}, {name: "4PM", value: 16}, {name: "5PM", value: 17}
+];
+
+console.log(timeSections[1].value)
+console.log(timeSections)
+
+$('#currentDay').text(dayjs().format('dddd, MMMM, D'));
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
